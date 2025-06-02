d8f163e33ca1de364696d288515e7feb8da13ef3
Alexander Gorbatov <57249660+businessmetric@users.noreply.github.com>
2025-06-02 10:23:22 +0300

    Update body_pass_vid_servme.js

--- body_pass_vid_servme.js
+++ body_pass_vid_servme.js
@@ -1,34 +1,26 @@
 function getCookie(name) {
     let matches = document.cookie.match(new RegExp(
-      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+        "(?:^|; )" + name.replace(/([\.$?*|{}$$$$$$$$\\\/\+^])/g, '\\$1') + "=([^;]*)"
     ));
     return matches ? decodeURIComponent(matches[1]) : undefined;
-  };   
+}
 
-// Получаем все ссылки, которые нужно обработать
+// Обрабатываем все ссылки, которые нужно изменить
 const links = document.querySelectorAll('a');
-const matchedLinks = [];
 
 links.forEach(link => {
-  const href = link.getAttribute('href');
-  if (href && href.includes('https://widget.servmeco.com/?oid=1574&source=venue_website')) {
-    matchedLinks.push(link);
-  }
-});
+    const href = link.getAttribute('href');
+    if (href && href.includes('widget.servmeco.com')) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // отменяем стандартное поведение ссылки
 
-// Обработчик для каждой ссылки
-matchedLinks.forEach(link => {
-  // Можно установить обработчик события 'click'
-  link.addEventListener('click', function (event) {
-    const veeneo_id = getCookie('veeneo_id');
-    if (typeof veeneo_id !== "undefined") {
-
-      event.preventDefault();
-
-      // Формируем новый URL
-      const newHref = `https://widget.servmeco.com/?oid=1574&source=venue_website&vid=${encodeURIComponent(veeneo_id)}`;
-      window.location.href = newHref;
+            const veeneo_id = getCookie('veeneo_id');
+            if (typeof veeneo_id !== "undefined") {
+                const newUrl = `https://widget.servmeco.com/?oid=1574&source=venue_website&vid=${encodeURIComponent(veeneo_id)}`;
+                // открываем в новой вкладке
+                window.open(newUrl, '_blank');
+            } else {                console.log('Кука veeneo_id не найдена.');
+            }
+        });
     }
-  
-  });
 });
