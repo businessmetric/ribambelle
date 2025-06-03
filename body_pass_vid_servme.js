b12761624c7b8ee04765d7ac413ead1d2326edfc
Alexander Gorbatov <57249660+businessmetric@users.noreply.github.com>
2025-06-03 10:07:36 +0300

    Update body_pass_vid_servme.js

--- body_pass_vid_servme.js
+++ body_pass_vid_servme.js
@@ -1,3 +1,4 @@
+/* DISABLE_START
 function getCookie(name) {
     let matches = document.cookie.match(new RegExp(
         "(?:^|; )" + name.replace(/([\.$?*|{}$$$$$$$$\\\/\+^])/g, '\\$1') + "=([^;]*)"
@@ -25,3 +26,4 @@ links.forEach(link => {
         });
     }
 });
+DISABLE_END */
