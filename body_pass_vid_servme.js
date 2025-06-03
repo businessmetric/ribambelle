fa98d0bcc5a9939ba4fc42cd9fbb44ea603e7f69
Hanna Tsilikina <130510435+HannaTsilikina@users.noreply.github.com>
2025-06-03 19:59:51 +0300

    Update body_pass_vid_servme.js

--- body_pass_vid_servme.js
+++ body_pass_vid_servme.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     function getCookie(name) {
         let matches = document.cookie.match(new RegExp(
@@ -15,14 +14,17 @@ document.addEventListener('DOMContentLoaded', function() {
             const veeneo_id = getCookie('veeneo_id');
             if (typeof veeneo_id !== "undefined") {
                 const newUrl = `https://widget.servmeco.com/?oid=1574&source=venue_website&vid=${encodeURIComponent(veeneo_id)}`;
-                // изменяем href сразу при загрузке
+                // изменяем href
                 link.setAttribute('href', newUrl);
+                // добавляем атрибут target="_blank" для открытия в новой вкладке
+                link.setAttribute('target', '_blank');
             } else {
                 console.log('Кука veeneo_id не найдена.');
             }
         }
     });
 });
+
 
 
 
