5c6059b0193886f4795443d53377f1d7aa8051a8
Hanna Tsilikina <130510435+HannaTsilikina@users.noreply.github.com>
2025-06-03 19:50:53 +0300

    Update body_pass_vid_servme.js

--- body_pass_vid_servme.js
+++ body_pass_vid_servme.js
@@ -1,31 +1,34 @@
 
-function getCookie(name) {
-    let matches = document.cookie.match(new RegExp(
-        "(?:^|; )" + name.replace(/([\.$?*|{}$$$$$$$$\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
-}
-
-// Обрабатываем все ссылки, которые нужно изменить
-const links = document.querySelectorAll('a');
-console.log(links)
-links.forEach(link => {
-  
-    const href = link.getAttribute('href');
-          console.log(link, href)
-    if (href && href.includes('widget.servmeco.com')) {
-        link.addEventListener('click', function(event) {
-            event.preventDefault(); // отменяем стандартное поведение ссылки
-
-            const veeneo_id = getCookie('veeneo_id');
-            if (typeof veeneo_id !== "undefined") {
-                const newUrl = `https://widget.servmeco.com/?oid=1574&source=venue_website&vid=${encodeURIComponent(veeneo_id)}`;
-                // открываем в новой вкладке
-                window.open(newUrl, '_blank');
-                 link.removeAttribute('href');
-            } else {                console.log('Кука veeneo_id не найдена.');
-            }
-        });
+document.addEventListener('DOMContentLoaded', function() {
+    function getCookie(name) {
+        let matches = document.cookie.match(new RegExp(
+            "(?:^|; )" + name.replace(/([\.$?*|{}\\\+\^])/g, '\\$1') + "=([^;]*)"
+        ));
+        return matches ? decodeURIComponent(matches[1]) : undefined;
     }
+
+    // Обрабатываем все ссылки, которые нужно изменить
+    const links = document.querySelectorAll('a');
+    console.log(links);
+    links.forEach(link => {
+        const href = link.getAttribute('href');
+        console.log(link, href);
+        if (href && href.includes('widget.servmeco.com')) {
+            link.addEventListener('click', function(event) {
+                event.preventDefault(); // отменяем стандартное поведение ссылки
+
+                const veeneo_id = getCookie('veeneo_id');
+                if (typeof veeneo_id !== "undefined") {
+                    const newUrl = `https://widget.servmeco.com/?oid=1574&source=venue_website&vid=${encodeURIComponent(veeneo_id)}`;
+                    // открываем в новой вкладке
+                    window.open(newUrl, '_blank');
+                    link.removeAttribute('href');
+                } else {
+                    console.log('Кука veeneo_id не найдена.');
+                }
+            });
+        }
+    });
 });
+
 
