5d11e1d0caf04c8dec30cf7089ed72b2c47f7e0a
Hanna Tsilikina <130510435+HannaTsilikina@users.noreply.github.com>
2025-06-03 19:57:28 +0300

    Update body_pass_vid_servme.js

--- body_pass_vid_servme.js
+++ body_pass_vid_servme.js
@@ -7,28 +7,22 @@ document.addEventListener('DOMContentLoaded', function() {
         return matches ? decodeURIComponent(matches[1]) : undefined;
     }
 
-    // Обрабатываем все ссылки, которые нужно изменить
     const links = document.querySelectorAll('a');
-    console.log(links);
+
     links.forEach(link => {
         const href = link.getAttribute('href');
-        console.log(link, href);
         if (href && href.includes('widget.servmeco.com')) {
-            link.addEventListener('click', function(event) {
-                event.preventDefault(); // отменяем стандартное поведение ссылки
-
-                const veeneo_id = getCookie('veeneo_id');
-                if (typeof veeneo_id !== "undefined") {
-                    const newUrl = `https://widget.servmeco.com/?oid=1574&source=venue_website&vid=${encodeURIComponent(veeneo_id)}`;
-                    // открываем в новой вкладке
-                    window.open(newUrl, '_blank');
-                    link.removeAttribute('href');
-                } else {
-                    console.log('Кука veeneo_id не найдена.');
-                }
-            });
+            const veeneo_id = getCookie('veeneo_id');
+            if (typeof veeneo_id !== "undefined") {
+                const newUrl = `https://widget.servmeco.com/?oid=1574&source=venue_website&vid=${encodeURIComponent(veeneo_id)}`;
+                // изменяем href сразу при загрузке
+                link.setAttribute('href', newUrl);
+            } else {
+                console.log('Кука veeneo_id не найдена.');
+            }
         }
     });
 });
+
 
 
